d4ae6183ae70591a41368f4d32d33ec3b41f969b
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-04-05 16:48:26 -0600

    refactored mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-const mobileBtn = document.querySelector(".header__hamburger-btn");
-const menu = document.querySelector(".header__nav-links");
+const hamburgerBtn = document.querySelector(".header__hamburger-btn");
+const navMenu = document.querySelector(".header__nav-links");
+const body = document.body;
+const screenSize = window.matchMedia("(min-width: 46.875em)");
 
-function toggleMenu() {
-  menu.classList.toggle("show");
-  console.log("Clicked!");
+function openCloseMenu() {
+  navMenu.classList.toggle("show");
+  body.classList.toggle("no-scroll");
+
+  // aria-expanded
+  const isOpen = navMenu.getAttribute("aria-expanded") === "true";
+  navMenu.setAttribute("aria-expanded", !isOpen);
 }
 
-mobileBtn.addEventListener("click", toggleMenu);
+function closeOnWindowResizing(e) {
+  if (e.matches) {
+    navMenu.classList.remove("show");
+    body.classList.remove("no-scroll");
+  }
+}
+
+closeOnWindowResizing(screenSize);
+hamburgerBtn.addEventListener("click", openCloseMenu);
+screenSize.addEventListener("change", closeOnWindowResizing);
